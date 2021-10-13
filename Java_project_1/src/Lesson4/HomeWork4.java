3b33397425b1756983837652da66e8340935a803
Alexey Eletsky <a.eletsky@infinnity.ru>
2021-10-13 22:57:00 +0500

    Добавлено 4 домашнее задание

--- Java_project_1/src/Lesson4/HomeWork4.java
+++ Java_project_1/src/Lesson4/HomeWork4.java
@@ -5,78 +5,147 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class HomeWork4 {
-    //☺ 1025
-    public static final String AI_WIN_MSG = "Победил компьютер!";
-    public static final String HUMMAN_WIN_MSG = "Победил человек!";
-    public static final String DRAW_MSG = "Ничья!";
+
+    private static final String AI_WIN_MSG = "Победил компьютер!";
+    private static final String HUMAN_WIN_MSG = "Победил человек!";
+    private static final String DRAW_MSG = "Ничья!";
 
     private static final char DOT_X = 'X';
-    private static final char DOT_O = '0';
+    private static final char DOT_O = 'O';
     private static final char DOT_EMPTY = '•';
-    private static final int SIZE = 3;
 
-    private static Scanner scanner = new Scanner(System.in);
+    private static final int SIZE = 5;
+    private static final int DOTS_TO_WIN = 4;
+
     private static char[][] map;
+    private static Scanner scanner;
+    private static Random random;
 
     public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+        random = new Random();
+
         initMap();
         printMap();
-        while (true) {
 
-            humanTurn();
-            printMap();
-
-            if (startGameLoop()) break;
-        }
+        startGameLoop();
     }
 
-    private static boolean startGameLoop() {
-        if (checkEndGame(DOT_X)) {
-            return true;
-        }
+    private static void startGameLoop() {
+        while (true) {
+            humanTurn();
+            printMap();
+            if (checkEndGame(DOT_X)) {
+                break;
+            }
 
-        aiTurn();
-        printMap();
-
-        if (checkEndGame(DOT_O)) {
-            return true;
+            aiTurn();
+            printMap();
+            if (checkEndGame(DOT_O)) {
+                break;
+            }
         }
-        return false;
     }
 
     private static boolean checkEndGame(char symbol) {
-        if (isMapFull()){
+        if (isMapFull()) {
             System.out.println(DRAW_MSG);
             return true;
         }
 
-        if (isWin(symbol)){
-            System.out.println(getWinMessage(symbol));
+        if (isWin(symbol)) {
+            System.out.println(getWinMessageBy(symbol));
             return true;
         }
         return false;
     }
 
-    private static boolean isWin(char symbol) {
-//        проверка строк
-        if (map[0][0] == symbol && map[0][1] == symbol && map[0][2] == symbol) return true;
-        if (map[1][0] == symbol && map[1][1] == symbol && map[1][2] == symbol) return true;
-        if (map[2][0] == symbol && map[2][1] == symbol && map[2][2] == symbol) return true;
-
-//        проверка полей
-        if (map[0][0] == symbol && map[1][0] == symbol && map[2][0] == symbol) return true;
-        if (map[0][1] == symbol && map[1][1] == symbol && map[2][1] == symbol) return true;
-        if (map[0][2] == symbol && map[1][2] == symbol && map[2][2] == symbol) return true;
-
-//        проверка диагоналей
-        if (map[0][0] == symbol && map[1][1] == symbol && map[2][2] == symbol) return true;
-        if (map[0][2] == symbol && map[1][1] == symbol && map[2][0] == symbol) return true;
-        return false;
-
+    private static String getWinMessageBy(char symbol) {
+        return symbol == DOT_X ? HUMAN_WIN_MSG : AI_WIN_MSG;
     }
 
-    private static String getWinMessage(char symbol){
-        return symbol == DOT_X ? HUMMAN_WIN_MSG : AI_WIN_MSG;
+    private static boolean isWin(char symbol) {
+        int countRow, countCol, countDiagonalA, countDiagonalB;
+
+        for (int row = 0; row < map.length; row++) {
+            countRow = 0;
+            countCol = 0;
+            countDiagonalA = 0;
+            countDiagonalB = 0;
+
+            for (int col = 0; col < map[row].length; col++) {
+                countRow = checkLine(symbol, countRow, row, col);
+                countCol = checkLine(symbol, countCol, col, row);
+                countDiagonalA = checkLine(symbol, countDiagonalA, col, col);
+                countDiagonalB = checkLine(symbol, countDiagonalB, map.length - col - 1, col);
+            }
+            if (countRow >= DOTS_TO_WIN || countCol >= DOTS_TO_WIN || countDiagonalA >= DOTS_TO_WIN || countDiagonalB >= DOTS_TO_WIN) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int checkLine(char symbol, int count, int row, int col) {
+        if (map[row][col] == symbol) {
+            count++;
+        } else if (map[row][col] != symbol && count < DOTS_TO_WIN) {
+            count = 0;
+        }
+        return count;
+    }
+
+
+    private static boolean isMapFull() {
+        for (int row = 0; row < map.length; row++) {
+            for (int col = 0; col < map[row].length; col++) {
+                if (isEmptyCell(row, col)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static void aiTurn() {
+        if (findWinningStep(DOT_O)) {
+            return;
+        } else if (findWinningStep(DOT_X)) {
+            return;
+        }
+
+        int row, col;
+        do {
+            row = random.nextInt(SIZE);
+            col = random.nextInt(SIZE);
+        } while (!isEmptyCell(row, col));
+
+        map[row][col] = DOT_O;
+        printAIStep(row, col);
+    }
+
+    private static void printAIStep(int row, int col) {
+        System.out.println("Компьютер сходил в координаты: " + (row + 1) + " " + (col + 1));
+    }
+
+    private static boolean findWinningStep(char symbol) {
+        for (int row = 0; row < map.length; row++) {
+            for (int col = 0; col < map.length; col++) {
+                if (map[row][col] == DOT_EMPTY) {
+                    map[row][col] = symbol;
+                    if (isWin(symbol)) {
+                        map[row][col] = DOT_O;
+                        printAIStep(row, col);
+                        return true;
+                    }
+                    if (!isWin(symbol)) {
+                        map[row][col] = DOT_EMPTY;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     private static void humanTurn() {
@@ -84,34 +153,34 @@ public class HomeWork4 {
         int row = 0;
         int col = 0;
         do {
-            row = reedIndex();
-            col = reedIndex();
+            row = readIndex();
+            col = readIndex();
 
             if (!checkRange(row) || !checkRange(col)) {
-                System.out.println("Координаты должны быть в диапазоне от 0 до " + SIZE);
+                System.out.println("Координаты должны быть в диапазоне от 1 до " + SIZE);
                 continue;
             }
 
-            if (isEmotyCell(row - 1, col - 1)){
+            if (isEmptyCell(row - 1, col - 1)) {
                 break;
-            }else {
+            } else {
                 System.out.println("Клетка уже занята!");
             }
+
         } while (true);
 
         map[row - 1][col - 1] = DOT_X;
     }
 
-    private static boolean isEmotyCell(int row, int col){
+    private static boolean isEmptyCell(int row, int col) {
         return map[row][col] == DOT_EMPTY;
     }
 
-    private static int reedIndex() {
-        while (!scanner.hasNextInt()){
-            System.out.println("Координаты должны иметь целочисленные значение!");
+    private static int readIndex() {
+        while (!scanner.hasNextInt()) {
+            System.out.println("Координаты должны иметь целочисленное значение!");
             scanner.next();
         }
-
         return scanner.nextInt();
     }
 
@@ -156,28 +225,5 @@ public class HomeWork4 {
             System.out.print(col + " ");
         }
         System.out.println();
-    }
-
-    private static void aiTurn() {
-        int row, col;
-        Random random = new Random();
-        do {
-            row = random.nextInt(SIZE);
-            col = random.nextInt(SIZE);
-        } while (!isEmotyCell(row, col));
-
-        map[row][col]  = DOT_O;
-    }
-
-    private static boolean isMapFull(){
-        for (int row = 0; row < SIZE; row++) {
-            for (int col = 0; col < SIZE; col++) {
-                if (isEmotyCell(row, col)){
-                    return false;
-                }
-            }
-        }
-
-        return true;
     }
 }
